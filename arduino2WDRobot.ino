ae2beeaa822c831353b5b6c6917c33fa1992989d
Fabien Maury <fab.maury@gmail.com>
2013-12-17 07:44:27 +0100

    improve AI: finest managment of collisions

--- arduino2WDRobot.ino
+++ arduino2WDRobot.ino
@@ -4,6 +4,10 @@
 #define motorRightSpeedPin 5
 #define motorLeftDirectionPin 7
 #define motorLeftSpeedPin 6
+#define BLOCKED 10
+#define NEAR_OBSTACLE 5
+#define FARAWAY_OBSTACLE 3
+#define NO_OBSTACLE 0
 
 void setup() 
 { 
@@ -15,22 +19,39 @@ void setup()
  
 void loop() 
 { 
-  //Serial.println(new String("distance:").concat(distance).concat(" cm"));
-  if(seeObstacle()){
-    pwmSpeed(200);
-    pwmForward();
-  }else{
-    pwmStop();
-  }
-  
+  switch(detect()){
+     case BLOCKED: 
+        pwmSpeed(150);
+        pwmBackward();
+        break;
+     case NEAR_OBSTACLE: 
+        pwmTurnLeft(90);
+        break;
+     case FARAWAY_OBSTACLE:
+         pwmTurnSmoothRight(30);
+         break;
+     default:
+        pwmSpeed(200);
+        pwmForward();
+    }
   delay(100); 
 }
 
-boolean seeObstacle(){
+int detect(){
   int distance=pingDistanceInCm();
   logger("distance: {} cm",distance);
-  return distance>20;
+  if(distance<10){
+      return BLOCKED;
+  }
+  if(distance<30){
+    return NEAR_OBSTACLE;
+  }
+  if(distance<50){
+    return FARAWAY_OBSTACLE;
+  }
+  return NO_OBSTACLE; 
 }
+
 
 
 
